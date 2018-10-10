daa87429332b1eadca9f4bbd0d832f8c891504ae
shivam2345 <32351363+shivam2345@users.noreply.github.com>
2018-10-10 12:53:38 -0700

    Updates

---
+++ precache-manifest.ed3a7d769fa315c5f4db48d4ebd8f721.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TimerApp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "24d50ba7f8e0e58d7ee1",
+    "url": "/TimerApp/static/js/runtime~main.24d50ba7.js"
+  },
+  {
+    "revision": "dda8e0a0aa8e65677a73",
+    "url": "/TimerApp/static/js/main.dda8e0a0.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/TimerApp/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "dda8e0a0aa8e65677a73",
+    "url": "/TimerApp/static/css/main.002bef53.chunk.css"
+  },
+  {
+    "revision": "fc31a05b6ba858f8a260749f86ffb8db",
+    "url": "/TimerApp/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/TimerApp/precache-manifest.ed3a7d769fa315c5f4db48d4ebd8f721.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/TimerApp/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
